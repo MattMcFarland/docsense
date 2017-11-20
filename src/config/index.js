6830dcf00cb8496d69cabf3261356e044ca677a8
Matt McFarland <contact@mattmcfarland.com>
2017-11-19 20:26:04 -0800

    update parser, use deepmerge for config and defaults

--- src/config/index.js
+++ src/config/index.js
@@ -2,11 +2,11 @@
 
 import cosmic from 'cosmiconfig'
 import defaultConfig from './default-config.json'
-
+import merge from 'deepmerge'
 /**
  * @returns {Promise<DocSenseConfig>}
  */
 export default (): Promise<DocSenseConfig> =>
   cosmic('docsense')
     .load(process.cwd())
-    .then(fileConfig => Object.assign({}, defaultConfig, fileConfig.config))
+    .then(({ config }) => merge(defaultConfig, config))
