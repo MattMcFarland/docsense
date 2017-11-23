43fbdeecf3c02dbdf4c2a8efba1d1bae69b7984d
Matt McFarland <contact@mattmcfarland.com>
2017-11-23 10:59:25 -0800

    add comments to selectors file

--- src/parser/selectors.js
+++ src/parser/selectors.js
@@ -1,9 +1,13 @@
 //@flow
 import { path } from 'ramda'
-import { TraverseContext } from 'traverse'
 
 const dotget = (dotpath: string): any => path(dotpath.split('.'))
-export default (ctx: TraverseContext): any => {
+
+/**
+ * A higher order function that applies selectors to the traversed context
+ * to make it easier to find things
+ */
+export default (ctx: any): any => {
   const node = ctx.node
   ctx.getFileName = () => dotget('loc.filename')(node)
   ctx.getRoot = () => (ctx.isRoot ? node : dotget('parents.0.node')(node))
