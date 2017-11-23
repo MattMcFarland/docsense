f44c64489b37333231b768c6b5ab9fd895850e58
Matt McFarland <contact@mattmcfarland.com>
2017-11-23 10:37:50 -0800

    cleanup parse engine, add annotations

--- src/parser/ParseEngine.js
+++ src/parser/ParseEngine.js
@@ -3,56 +3,75 @@
 import { EventEmitter } from 'events'
 import traverse from 'traverse'
 import withSelectors from './selectors'
-import doctrine from 'doctrine'
+import Doctrine, { Syntax } from 'doctrine'
+import Babylon from 'babylon'
 
 interface Parser {
-  parse(str: string, options: ParseOptions): any;
+  parse(str: string, options: ParseOptions): File;
 }
-
+type LeadingComment = {
+  value: string,
+  type: Doctrine.Type,
+}
+type ASTNode = {
+  type: string,
+  leadingComments: LeadingComment[],
+  __doc_tags__: Doctrine.Type[],
+}
 export default class ParseEngine extends EventEmitter {
-  constructor(parserName: string, parseOptions?: ParseOptions) {
+  parser: Parser
+  parserName: string
+  parseOptions: ParseOptions
+  doctrine: Doctrine
+  constructor(parserName: string, parseOptions?: ParseOptions = {}) {
     super()
     this.parserName = parserName
-    this.parser = ((module.require(parserName): any): Parser)
+    this.parser = (module.require(parserName): Parser)
     this.parseOptions = parseOptions
   }
   addFile(fileName: string, data: string): void {
-    const ast: any = this.parse(data, { sourceFilename: fileName })
+    const ast: File = this.parse(data, { sourceFilename: fileName })
     this.emitNodes(ast)
   }
-  parse(data: string, options?: ParseOptions): void {
+  parse(data: string, options?: ParseOptions): File {
     return this.parser.parse(data, { ...options, ...this.parseOptions })
   }
-  emitNodes(ast: any) {
+  emitNodes(ast: File) {
     // traverse uses keyword 'this' for context to the node itself,
     // so we dont use an arrow function, and we alias parseEngine
     const parseEngine = this
-    traverse(ast).forEach(function(value: any): void {
+    traverse(ast).forEach(function(value: ASTNode) {
       if (value && value.type) {
-        maybeInjectTags(value)
+        parseEngine.maybeInjectTags(value)
         parseEngine.emit(value.type, withSelectors(this), value)
       }
     })
   }
-}
-
-function maybeInjectTags(node: any): void {
-  if (node && node.type) {
-    if (Array.isArray(node.leadingComments)) {
-      node.__doc_tags__ = node.leadingComments.reduce(
-        (acc: any[], leadingComment: any, index: number) => {
-          if (leadingComment.type === 'CommentBlock') {
-            return [].concat(
-              doctrine.parse(leadingComment.value, {
-                unwrap: true,
-                sloppy: true,
-              }),
-              acc
-            )
-          }
-        },
-        []
-      )
+  maybeInjectTags(node: ASTNode): void {
+    if (node && node.type) {
+      if (Array.isArray(node.leadingComments)) {
+        node.__doc_tags__ = this.injectTags(node.leadingComments)
+      }
     }
+  }
+  /**
+   *
+   * @param {*} leadingComments
+   */
+  injectTags(leadingComments: LeadingComment[]) {
+    const parseCommentTags = (content: string): Doctrine.Type[] =>
+      Doctrine.parse(content, {
+        unwrap: true,
+        sloppy: true,
+      })
+    const tagInjectReducer = (
+      acc: Doctrine.Type[],
+      leadingComment: LeadingComment
+    ) =>
+      (leadingComment.type === 'CommentBlock' &&
+        [].concat(parseCommentTags(leadingComment.value), acc)) ||
+      acc
+
+    return leadingComments.reduce(tagInjectReducer, [])
   }
 }
--- src/parser/selectors.js
+++ src/parser/selectors.js
@@ -1,8 +1,9 @@
 //@flow
 import { path } from 'ramda'
+import { TraverseContext } from 'traverse'
 
 const dotget = (dotpath: string): any => path(dotpath.split('.'))
-export default (ctx: any): any => {
+export default (ctx: TraverseContext): any => {
   const node = ctx.node
   ctx.getFileName = () => dotget('loc.filename')(node)
   ctx.getRoot = () => (ctx.isRoot ? node : dotget('parents.0.node')(node))
