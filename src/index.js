7558322c6782b72642e13be80875b88132099a10
Matt McFarland <contact@mattmcfarland.com>
2017-11-25 09:16:08 -0500

    add poc fn to "build database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 // @flow
 import './logger'
 import getConfig from './config'
-import { fatalError } from './utils/common'
+import { fatalError, logContext } from './utils/common'
 import { setupCorePlugins } from './utils/plugin'
 import { parseFiles } from './utils/parse'
 
 getConfig()
   .then(setupCorePlugins)
   .then(parseFiles)
+  .then(buildDatabase)
+  .then(logContext)
   .catch(fatalError)
+
+function buildDatabase(db) {
+  const { file_collection, export_collection } = db.getState()
+  const filesWithExports = file_collection.reduce((acc, file) => {
+    const fileExports = export_collection.filter(
+      xp => xp.file_id == file.file_id
+    )
+    acc.push({
+      file_id: file.file_id,
+      exports: fileExports.map(x => x.export_id),
+    })
+    return acc
+  }, [])
+  return filesWithExports
+}
