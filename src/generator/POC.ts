e6b2b7779acece74e6fe80596d4f1d34caaac89e
Matt McFarland <contact@mattmcfarland.com>
2017-12-05 09:16:29 -0500

    add function documentation generation

--- src/generator/POC.ts
+++ src/generator/POC.ts
@@ -1,14 +1,20 @@
+import * as mkdirp from 'mkdirp';
+import { resolve as resolvePath } from 'path';
+import { promisify } from 'util';
+
 import { compile, require_template } from './compiler';
+
+const mkdir = promisify(mkdirp);
 
 const db = require('../../docs/db.json');
 
 // create index html
-const { file_collection, export_collection } = db;
-const filesWithExports = file_collection.reduce((acc: any, file: any) => {
+const { file_collection, export_collection, function_collection } = db;
+const esModules = file_collection.reduce((acc: any, file: any) => {
   const fileExports = export_collection.filter(
     (xp: any) => xp.file_id === file.file_id
   );
-  const exports = fileExports.map((x: any) => x.export_id);
+  const exports = fileExports.map((x: any) => x);
   if (exports.length) {
     acc.push({
       file_id: file.file_id,
@@ -18,6 +24,44 @@ const filesWithExports = file_collection.reduce((acc: any, file: any) => {
   return acc;
 }, []);
 
-const indexPage = require_template('./templates/index.hbs');
+const distDir = require('path').resolve(__dirname, '../../docs');
 
-compile(indexPage, { filesWithExports }, 'index.html');
+const makeModuleDirs = (): Promise<any> =>
+  Promise.all(
+    esModules.map((esm: any) => mkdir(resolvePath(distDir, esm.file_id)))
+  );
+const makeModuleLinks = () =>
+  esModules.map((esm: any) => {
+    return { link: esm.file_id + '/index.html' };
+  });
+
+const getModuleInfo = (esm: any) => {
+  //console.log(JSON.stringify(esm, null, 2));
+  const exports = esm.exports.map((exm: any) => {
+    if (exm.function_id) {
+      const withFn = function_collection.find(
+        (fns: any) => fns.function_id === exm.function_id
+      );
+      console.log(withFn);
+      return {
+        file_id: esm.file_id,
+        export_id: exm.export_id,
+        function: withFn,
+      };
+    }
+  });
+  return { exports };
+};
+
+makeModuleDirs().then(() => {
+  esModules.forEach((esm: any) => {
+    const esModulePage = require_template('./templates/esModule.hbs');
+    compile(
+      esModulePage,
+      { esModule: getModuleInfo(esm), esModules },
+      esm.file_id + '/index.html'
+    );
+  });
+  const indexPage = require_template('./templates/index.hbs');
+  compile(indexPage, { esModules }, 'index.html');
+});
--- src/generator/compiler.ts
+++ src/generator/compiler.ts
@@ -8,17 +8,17 @@ export const require_template = (relPath: string) => {
   return readFileSync(targetPath, 'utf8');
 };
 
-export const layout = (page: string) => {
+export const layout = (page: string, data: any) => {
   const layout_source = require_template('./templates/_layout.hbs');
   const template = Handlebars.compile(layout_source);
-  const result = template({ page });
+  const result = template({ page, ...data });
   return result;
 };
 
 export const compile = (source: string, data: any, target: string) => {
   const template = Handlebars.compile(source);
   const content = template(data);
-  const withLayout = layout(content);
+  const withLayout = layout(content, data);
   const targetPath = path.resolve(process.cwd(), 'docs', target);
   writeFileSync(targetPath, withLayout, 'utf8');
 };
