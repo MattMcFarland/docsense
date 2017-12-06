05d6236ce0047904a4fe93900f3cc204b43ac2cc
Matt McFarland <contact@mattmcfarland.com>
2017-12-06 17:01:32 -0500

    fix generator to not use parens where fns do not exist, update settings

--- src/generator/templates/_layout.ts
+++ src/generator/templates/_layout.ts
@@ -8,16 +8,15 @@ module.exports = ({ content, data }: any) => `
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css">
   <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/default.min.css">
+  <script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
   <style>
     html * {
       font-family: 'Open Sans', sans-serif;
       color: #444;
     }
   </style>
-  <link rel="stylesheet"
-  href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/default.min.css">
-  <script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/highlight.min.js"></script>
-  <script>hljs.initHighlightingOnLoad();</script>
 </head>
 
 <body class="bg-black-10">
