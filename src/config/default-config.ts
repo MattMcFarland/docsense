7ea3d3429f2d62581b2d1b9a4df55829df43050b
Matt McFarland <contact@mattmcfarland.com>
2017-12-08 09:05:17 -0500

    update schema and options

--- src/config/default-config.ts
+++ src/config/default-config.ts
@@ -4,7 +4,6 @@ const DefaultConfig: DocSenseConfig = {
   parseOptions: {
     tokens: false,
   },
-  useCorePlugins: true,
   files: [],
   main: './readme',
 };
@@ -13,29 +12,69 @@ export default DefaultConfig;
 
 /** @interface ParseOptions parseOptions */
 export interface ConfigParseOptions {
-  /** @property {boolean} allowImportExportEverywhere - By default, import and export declarations can only appear at a program's top level. Setting this option to true allows them anywhere where a statement is allowed. */
+  /**
+   * By default, import and export declarations can only appear at a program's top level. Setting this option to true allows them anywhere where a statement is allowed.
+   * @TJS-TYPE bool
+   */
   allowImportExportEverywhere?: boolean;
-  /** @property {boolean} allowImportExportEverywhere - By default, a return statement at the top level raises an error. Set this to true to accept such code. */
+  /**
+   *  By default, a return statement at the top level raises an error. Set this to true to accept such code.
+   * @TJS-TYPE bool
+   */
   allowReturnOutsideFunction?: boolean;
-  /** @property {SourceType} sourceType - Indicate the mode the code should be parsed in. Can be one of "script", "module", or "unambiguous". Defaults to "script". "unambiguous" will make Babylon attempt to guess, based on the presence of ES6 import or export statements. Files with ES6 imports and exports are considered "module" and are otherwise "script". */
+  /**
+   * Indicate the mode the code should be parsed in. Can be one of "script", "module", or "unambiguous". Defaults to "script". "unambiguous" will make Babylon attempt to guess, based on the presence of ES6 import or export statements. Files with ES6 imports and exports are considered "module" and are otherwise "script".
+   *
+   */
   sourceType?: 'script' | 'module' | 'unambiguous';
-  /** @property {string} sourceFilename - Correlate output AST nodes with their source filename. Useful when generating code and source maps from the ASTs of multiple input files. */
+  /**
+   * Correlate output AST nodes with their source filename.
+   * Useful when generating code and source maps from the ASTs of multiple input files.
+   * @TJS-TYPE string
+   */
   sourceFilename?: string;
-  /** @property {number} startLine -  By default, the first line of code parsed is treated as line 1. You can provide a line number to alternatively start with. Useful for integration with other source tools. */
+  /**
+   * By default, the first line of code parsed is treated as line 1. You can provide a line number to alternatively start with. Useful for integration with other source tools.
+   * @property {number} startLine -
+   * @TJS-TYPE integer
+   */
   startLine?: number;
-  /** @property {string[]} plugins - Array containing the plugins that you want to enable. */
+  /**
+   * @property {string[]} plugins - Array containing the plugins that you want to enable.
+   * @TJS-TYPE string
+   */
   plugins?: string[];
-  /** @property {[number, number]} ranges - Adds a ranges property to each node: [node.start, node.end] */
+  /**
+   * Adds a ranges property to each node: [node.start, node.end]
+   *
+   */
   ranges?: [number, number];
-  /** @property {boolean} tokens - Adds all parsed tokens to a tokens property on the File node */
+  /**
+   * Adds all parsed tokens to a tokens property on the File node
+   * @TJS-TYPE bool
+   */
   tokens?: boolean;
 }
 
 export interface DocSenseConfig {
+  /**
+   * Array of files or glob patterns of which you want to parse for documentation
+   */
   files: string[];
-  parser: string;
+  /**
+   * Parser, currently 'babylon' is only supported
+   */
+  parser: 'babylon';
+  /**
+   * Options passed to the parser
+   */
   parseOptions?: ConfigParseOptions;
-  useCorePlugins?: boolean;
+  /**
+   * Path of which the documentation will be generated.
+   */
   out?: string;
+  /**
+   * What markdown file to load for the main page. (e.g., './readme')
+   */
   main: string;
 }
