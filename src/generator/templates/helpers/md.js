cf26d00147a97a3eac7ea389d9135f0876d7f741
Matt McFarland <contact@mattmcfarland.com>
2017-12-11 02:30:32 -0500

    use marked for markdown, created md helper. now all markdown will be rendered the same

---
+++ src/generator/templates/helpers/md.js
@@ -0,0 +1,13 @@
+const Handlebars = require('handlebars');
+const marked = require('marked');
+const addEmojis = require('../../compiler').addEmojis;
+const markedStyle = require('../../marked/renderer').default;
+const renderer = markedStyle();
+
+marked.setOptions({ renderer });
+
+module.exports = function(ctx) {
+  const str = ctx.fn(this);
+  const parsed = marked(addEmojis(str));
+  return new Handlebars.SafeString(parsed);
+};
