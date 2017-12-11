9b957738a8bd9dff7d44a93f6e561bdcf5010554
Matt McFarland <contact@mattmcfarland.com>
2017-12-11 02:39:54 -0500

    Remove dead code

--- src/generator/templates/helpers/fnSignature.js
+++ src/generator/templates/helpers/fnSignature.js
@@ -1,10 +1,5 @@
 const Handlebars = require('handlebars');
 const renderParam = require('./renderParam');
-// {{~#each function.params as |param|~}}
-// <span class="dark-red">
-//   {{~#if @last~}}{{~renderParam param~}}{{else}}{{~renderParam param~}}<span class="black-50">, </span>{{~/if~}}
-// </span>
-// {{~/each~}})
 
 module.exports = (fn, jsdoc) => {
   const params = fn.params.map(renderParam);
