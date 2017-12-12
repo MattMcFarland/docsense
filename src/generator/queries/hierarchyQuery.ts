cb70d13eada9ae8e965ba8999949fb1c91709d3c
Matt McFarland <contact@mattmcfarland.com>
2017-12-11 19:51:25 -0500

    add tree traversal to dir hierarchy

--- src/generator/queries/hierarchyQuery.ts
+++ src/generator/queries/hierarchyQuery.ts
@@ -1,3 +1,7 @@
+import * as Path from 'path';
+import * as traverse from 'traverse';
+
+import { Traverse, TraverseContext } from '../../_types/external/traverse';
 import { ESModule } from '../../core-plugins/es-modules';
 import { FileKind, FileModel } from '../../core-plugins/file';
 import { decode, encode } from '../../utils/base64';
@@ -6,11 +10,11 @@ import Query from './Query';
 export interface ObjectTree {
   [key: string]: ObjectTree;
 }
+interface Records {
+  file_collection: FileModel[];
+}
 
-const hierarchyQuery = (db: Lowdb): Promise<ObjectTree> => {
-  interface Records {
-    file_collection: FileModel[];
-  }
+const hierarchyQuery = (db: Lowdb): Promise<Traverse<ObjectTree>> => {
   const { file_collection }: Records = db.getState();
   const treeInject = (obj: ObjectTree, path: string[]): void => {
     if (path.length === 0) return;
@@ -22,7 +26,23 @@ const hierarchyQuery = (db: Lowdb): Promise<ObjectTree> => {
   file_collection.forEach(file => {
     treeInject(hierarchy, file.path.split('/'));
   });
-  return Promise.resolve(hierarchy);
+
+  const tree = traverse(hierarchy);
+  // tslint:disable-next-line:only-arrow-functions
+  tree.forEach(function(value: ObjectTree) {
+    // tslint:disable-next-line:no-this-assignment
+
+    if (this.isLeaf) {
+      const fullpath = this.path.join('/');
+      const { dir, name } = Path.parse(fullpath);
+      const filepath = `${dir}/${name}`;
+      const file_id = encode(filepath);
+      const filedata = file_collection.find(f => f.id === file_id);
+      this.node = filedata;
+    }
+  });
+
+  return Promise.resolve(tree);
 };
 
-export default new Query<ObjectTree, void>(hierarchyQuery);
+export default new Query<Traverse<ObjectTree>, void>(hierarchyQuery);
