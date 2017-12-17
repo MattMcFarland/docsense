dcd99f39306ef072cd7e4153e5bc1fc96dba3f9a
Matt McFarland <contact@mattmcfarland.com>
2017-12-17 06:26:38 -0500

    escape angled brackets

--- src/generator/marked/styles/code.ts
+++ src/generator/marked/styles/code.ts
@@ -1,13 +1,15 @@
 import * as highlightjs from 'highlight.js';
 export default function(code: string, language: string) {
-  code.replace('<', '&lt;').replace('>', '&gt;');
+  const _code = code.replace('<', '&lt;').replace('>', '&gt;');
   if (!language)
-    return `<pre><code class="hljs black-60 no-highlight">${code}</code></pre>`;
+    return `<pre><code class="hljs black-60 no-highlight">${_code}</code></pre>`;
 
   // Check whether the given language is valid for highlight.js.
   const validLang = !!(language && highlightjs.getLanguage(language));
   // Highlight only if the language is valid.
-  const highlighted = validLang ? highlightjs.highlightAuto(code).value : code;
+  const highlighted = validLang
+    ? highlightjs.highlightAuto(_code).value
+    : _code;
 
   return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
 }
