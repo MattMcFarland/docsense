129dfa2077d11f34c7c140c3fd4c413741eea84f
Matt McFarland <contact@mattmcfarland.com>
2017-12-27 17:58:53 -0500

    add comments

--- src/generator/queries/Query.ts
+++ src/generator/queries/Query.ts
@@ -1,14 +1,25 @@
-import getConfig, { DocSenseConfig } from '../../config';
 import { connect as dbConnect, Lowdb } from '../../storage/db';
 
+/**
+ * Creates a Query of the database
+ */
 class Query<A, B> {
   private db: Lowdb;
   private resolver: (db: Lowdb, ...args: B[]) => Promise<A>;
 
+  /**
+   * The resolver function that will be called when you run exec()
+   * @param resolver {Function (db, ...args)}
+   */
   constructor(resolver: (db: Lowdb, ...args: B[]) => Promise<A>) {
     this.resolver = resolver;
   }
 
+  /**
+   * Runs the query asynchronously, returning a promise.
+   * @param args args passed to exec, if any.
+   * @returns Promise<A>
+   */
   public exec(...args: B[]): Promise<A> {
     return this.connect().then(db => this.resolver(db, ...args));
   }
